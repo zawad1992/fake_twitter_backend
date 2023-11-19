babf3f64a1a82bc185506975a2a04ceddab6c9ca
Zawadul Kawum <zawad1992@gmail.com>
2023-11-19 11:20:14 +0600

    Add routes for authentication and tweets

--- engine/routes/api.php
+++ engine/routes/api.php
@@ -3,17 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TweetsController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
+// Protected routes
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+
+
+    Route::get('/tweets', [TweetsController::class, 'index']);
+    Route::post('/tweets', [TweetsController::class, 'store']);
+    Route::get('/tweets/{id}', [TweetsController::class, 'show']);
+    Route::put('/tweets/{id}', [TweetsController::class, 'update']);
+    Route::delete('/tweets/{id}', [TweetsController::class, 'destroy']);
 });
