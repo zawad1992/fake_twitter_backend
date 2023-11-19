63b9537b4f6fa7d44900bde88e70881d029d389d
Zawadul Kawum <zawad1992@gmail.com>
2023-11-19 16:19:08 +0600

    Add new routes for followers and likes

--- engine/routes/api.php
+++ engine/routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TweetsController;
+use App\Http\Controllers\FollowersController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\LikesController;
 
 use Illuminate\Support\Facades\DB;
 
@@ -32,11 +35,21 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-
+    
+    Route::get('/users', [UsersController::class, 'index']);
 
     Route::get('/tweets', [TweetsController::class, 'index']);
     Route::post('/tweets', [TweetsController::class, 'store']);
     Route::get('/tweets/{id}', [TweetsController::class, 'show']);
     Route::put('/tweets/{id}', [TweetsController::class, 'update']);
     Route::delete('/tweets/{id}', [TweetsController::class, 'destroy']);
+
+    Route::get('/followers', [FollowersController::class, 'index']);
+    Route::get('/followers/{id}', [FollowersController::class, 'followers']);
+    Route::post('/follow/{id}/{status}', [FollowersController::class, 'follow']); // 1 = follow, 0 = unfollow
+
+    Route::get('/likes', [LikesController::class, 'index']);
+    Route::get('/likes/{tweetid}', [LikesController::class, 'likes']);
+    Route::post('/like/{tweetidid}/{status}', [LikesController::class, 'like']); // 1 = like, 0 = unlike
+   
 });
