5da88cbdebbb76fa3a61052c8ebe7157c267a2db
Zawadul Kawum <zawad1992@gmail.com>
2023-11-22 21:07:50 +0600

    Add trendingTweets method to TweetsController

--- engine/app/Http/Controllers/TweetsController.php
+++ engine/app/Http/Controllers/TweetsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use App\Models\Follower;
 
 class TweetsController extends Controller
 {
@@ -48,6 +49,38 @@ class TweetsController extends Controller
                             ->where('user_id', $userid)
                             ->orderBy('created_at', 'desc')
                             ->paginate(5); // paginate with 5 items per page
+            return response()->json([
+                'status' => 'success',
+                'data' => $dataObj,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong',
+            ], 500);
+        }        
+    }
+
+    public function trandingTweets($userid = null)
+    {
+        try {
+            // Subquery to find the IDs of users followed by the given user
+            $followedUserIds = Follower::where('follower_id', $userid)->pluck('followed_id')->toArray();
+
+            $tweetModel = new Tweet();
+            $dataObj = $tweetModel
+                            ->select('_id', 'user_id', 'tweet', 'created_at')
+                            ->with([
+                                'user' => function ($query) {
+                                    $query->select('name', 'email');
+                                }
+                            ])
+                            ->where('user_id','<>',$userid)
+                            ->whereNotIn('user_id', $followedUserIds)
+                            ->orderBy('created_at', 'desc')
+                            ->limit(5)
+                            ->get(); // Get top 5 trending tweets
+
             return response()->json([
                 'status' => 'success',
                 'data' => $dataObj,
